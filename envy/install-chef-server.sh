98a1d0dc9730e08f5440849f91958e6da42e41dc
Michael Bascus <mbascus@gmail.com>
2013-04-24 01:20:50 -0700

    specify floating ip for nginx url

--- envy/install-chef-server.sh
+++ envy/install-chef-server.sh
@@ -4,6 +4,9 @@
 wget https://opscode-omnibus-packages.s3.amazonaws.com/ubuntu/12.04/x86_64/chef-server_11.0.8-1.ubuntu.12.04_amd64.deb
 sudo dpkg -i chef-server_11.0.8-1.ubuntu.12.04_amd64.deb
 sudo chef-server-ctl reconfigure
+floating_ip=`curl http://169.254.169.254/2009-04-04/meta-data/public-ipv4`
+echo "nginx['url'] = \"https://$floating_ip\"" | sudo tee /etc/chef-server/chef-server.rb
+sudo chef-server-ctl reconfigure
 # verify it's running
 sudo  chef-server-ctl service-list
 sudo  chef-server-ctl status
